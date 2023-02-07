f0d3c97bdce4f17414536ecc6ac85ea5029df4e7
Kieran Grant <kierang92@gmail.com>
2023-02-07 11:22:40 +0000

    Add style VAE

---
+++ src/models/style_transfer_vae.py
@@ -0,0 +1,394 @@
+from argparse import ArgumentParser
+from math import prod
+from typing import Tuple
+
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from pedalboard.pedalboard import load_plugin
+
+from src.dataset.paired_audio_dataset import PairedAudioDataset
+from src.wrappers.dafx_wrapper import DAFXWrapper
+from src.wrappers.null_dafx_wrapper import NullDAFXWrapper
+
+
+class StyleTransferVAE(pl.LightningModule):
+    # =========== MAGIC METHODS =============
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.save_hyperparameters()
+
+        self._build_model()
+
+    # =========== PRIVATE METHODS =============
+    def _build_model(self):
+        self._build_spectrogram_parameters()
+        self._build_vae_parameters()
+        self._build_dafx()
+        self._build_encoder()
+        self._build_decoder()
+
+    def _build_spectrogram_parameters(self):
+        self.window = torch.nn.Parameter(torch.hann_window(self.hparams.window_size))
+
+    def _build_vae_parameters(self):
+        self.hidden_dim_enc = prod(self.hparams.hidden_dim)
+        self.hidden_dim_dec = self.hparams.hidden_dim
+
+        channels = [self.hparams.num_channels, 8, 16, 32]
+        self.enc_channels = channels
+        self.dec_channels = channels[::-1]
+
+    def _build_dafx(self):
+        # Load instances for each type of DAFX
+        self.dafx_list = self._get_dafx_from_names()
+        # Create entry for current dafx name for logging
+        self.current_dafx = None
+
+    def _build_encoder(self):
+        conv_layers = []
+
+        for i in range(len(self.enc_channels) - 1):
+            conv_layers.append(nn.Sequential(
+                nn.Conv2d(in_channels=self.enc_channels[i],
+                          out_channels=self.enc_channels[i + 1],
+                          kernel_size=self.hparams.conv_kernel,
+                          padding=self.hparams.conv_padding,
+                          stride=self.hparams.conv_stride
+                          ),
+                nn.ReLU(),
+                nn.BatchNorm2d(self.enc_channels[i + 1])
+            ))
+
+        self.encoder_conv = nn.Sequential(*conv_layers)
+
+        self.mu = nn.Linear(self.hidden_dim_enc, self.hparams.latent_dim)
+        self.log_var = nn.Linear(self.hidden_dim_enc, self.hparams.latent_dim)
+
+    def _build_decoder(self):
+        self.decoder_linear = nn.Sequential(
+            nn.Linear(in_features=self.hparams.latent_dim, out_features=self.hidden_dim_enc),
+            nn.ReLU())
+
+        conv_layers = []
+
+        for i in range(len(self.dec_channels) - 2):
+            conv_layers.append(nn.Sequential(
+                nn.Conv2d(in_channels=self.dec_channels[i],
+                          out_channels=self.dec_channels[i + 1],
+                          kernel_size=self.hparams.conv_kernel,
+                          padding=self.hparams.conv_padding,
+                          stride=self.hparams.conv_stride
+                          ),
+                nn.ReLU(),
+                nn.BatchNorm2d(self.dec_channels[i + 1])
+            ))
+
+        conv_layers.append(nn.Sequential(
+            nn.Conv2d(in_channels=self.dec_channels[-2],
+                      out_channels=self.dec_channels[-1],
+                      kernel_size=self.hparams.conv_kernel,
+                      padding=self.hparams.conv_padding,
+                      stride=self.hparams.conv_stride
+                      )))
+
+        self.decoder_conv = nn.Sequential(*conv_layers)
+
+    @staticmethod
+    def _calculate_kl_loss(mean, log_variance):
+        # calculate KL divergence
+        kld_batch = -0.5 * torch.sum(1 + log_variance - torch.square(mean) - torch.exp(log_variance), dim=1)
+        kld = torch.mean(kld_batch)
+
+        return kld
+
+    def _calculate_reconstruction_loss(self, x, x_hat):
+        if self.hparams.recon_loss.lower() == "mse":
+            return F.mse_loss(x, x_hat, reduction="mean")
+        elif self.hparams.recon_loss.lower() == "l1":
+            return F.l1_loss(x, x_hat, reduction="mean")
+        elif self.hparams.recon_loss.lower() == "bce":
+            return F.binary_cross_entropy(x, x_hat, reduction="mean")
+        else:
+            raise NotImplementedError
+
+    def calculate_loss(self, mean, log_variance, predictions, targets):
+        r_loss = self._calculate_reconstruction_loss(targets, predictions)
+        kl_loss = self._calculate_kl_loss(mean, log_variance)
+        return r_loss, kl_loss
+
+    def _get_dafx_from_names(self):
+        dafx_instances = []
+
+        for dafx_name in self.hparams.dafx_names:
+            if dafx_name.lower() == "clean":
+                dafx_instances.append(NullDAFXWrapper())
+            else:
+                dafx = load_plugin(self.hparams.dafx_file, plugin_name=dafx_name)
+                dafx_instances.append(DAFXWrapper(dafx, sample_rate=self.hparams.sample_rate))
+
+        return dafx_instances
+
+    def _get_dafx_for_current_epoch(self, current_epoch: int):
+        # Use mod arithmetic to cycle through dafx
+        idx = current_epoch % len(self.dafx_list)
+
+        self.current_dafx = self.hparams.dafx_names[idx]
+
+        print(f"\nEpoch {current_epoch} using DAFX: {self.current_dafx}")
+
+        return self.dafx_list[idx]
+
+    def encode(self, x):
+        x = self.encoder_conv(x)
+
+        x = x.view(-1, self.hidden_dim_enc)
+
+        mu = self.mu(x)
+        log_var = self.log_var(x)
+
+        return mu, log_var
+
+    def decode(self, z):
+        x = self.decoder_linear(z)
+
+        x = x.view(-1, *self.hidden_dim_dec)
+
+        x = self.decoder_conv(x)
+
+        return x
+
+    @staticmethod
+    def reparameterise(mu, log_var):
+        std = torch.exp(log_var / 2)
+        eps = torch.randn_like(std)
+        return mu + std * eps
+
+    def forward(self, x):
+        mu, log_var = self.encode(x)
+        z = self.reparameterise(mu, log_var)
+        out = self.decode(z)
+
+        return out, mu, log_var
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
+
+    def common_paired_step(
+            self,
+            batch: Tuple,
+            batch_idx: int,
+            train: bool = False,
+    ):
+        # Get audio
+        x, y = batch
+
+        # Get spectrograms
+        x_s = self.audio_to_spectrogram(signal=x,
+                                        n_fft=self.hparams.n_fft,
+                                        hop_length=self.hparams.hop_length,
+                                        return_complex=self.hparams.return_complex
+                                        )
+
+        y_s = self.audio_to_spectrogram(signal=y,
+                                        n_fft=self.hparams.n_fft,
+                                        hop_length=self.hparams.hop_length,
+                                        return_complex=self.hparams.return_complex
+                                        )
+
+        X = torch.concat([x_s, y_s], dim=1)
+
+        # Get reconstruction as well as mu, var
+        X_hat, X_mu, X_log_var = self(X)
+
+        # Calculate recon losses for clean/effected signals
+        r_loss, kl_loss = self.calculate_loss(X_mu, X_log_var, X_hat, X)
+
+        # Total loss is additive
+        loss = r_loss + (self.hparams.vae_beta * kl_loss)
+
+        # log the losses
+        self.log(("train" if train else "val") + "_loss/loss", loss)
+        self.log(("train" if train else "val") + "_loss/reconstruction_loss", r_loss)
+        self.log(("train" if train else "val") + "_loss/kl_divergence", kl_loss)
+
+        return loss
+
+    def audio_to_spectrogram(self,
+                             signal: torch.Tensor,
+                             n_fft: int = 4096,
+                             hop_length: int = 2048,
+                             return_complex: bool = True):
+
+        bs, _, _ = signal.size()
+
+        if return_complex:
+            # compute spectrogram of waveform
+            X = torch.stft(
+                signal.view(bs, -1),
+                n_fft=n_fft,
+                hop_length=hop_length,
+                window=self.window,
+                return_complex=False,
+            )
+
+            return X.permute(0, 3, 2, 1)
+
+        X = torch.stft(
+            signal.view(bs, -1),
+            n_fft=n_fft,
+            hop_length=hop_length,
+            window=self.window,
+            return_complex=True,
+        )
+
+        X_abs = X.abs().unsqueeze(1).permute(0, 1, 3, 2)
+
+        return X_abs
+
+    def training_step(self, batch, batch_idx):
+        loss = self.common_paired_step(
+            batch,
+            batch_idx,
+            train=True,
+        )
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss = self.common_paired_step(
+            batch,
+            batch_idx,
+            train=False,
+        )
+
+        return loss
+
+    def train_dataloader(self):
+        # Return dataloader based on epoch??
+        dafx = self._get_dafx_for_current_epoch(self.current_epoch)
+
+        train_dataset = PairedAudioDataset(
+            dafx=dafx,
+            audio_dir=self.hparams.audio_dir,
+            subset="train",
+            train_frac=self.hparams.train_frac,
+            half=self.hparams.half,
+            length=self.hparams.train_length,
+            input_dirs=self.hparams.input_dirs,
+            buffer_size_gb=self.hparams.buffer_size_gb,
+            buffer_reload_rate=self.hparams.buffer_reload_rate,
+            num_examples_per_epoch=self.hparams.train_examples_per_epoch,
+            effect_input=self.hparams.effect_input,
+            effect_output=self.hparams.effect_output,
+            random_effect_threshold=self.hparams.random_effect_threshold,
+            augmentations={
+                "pitch": {"sr": self.hparams.sample_rate},
+                "tempo": {"sr": self.hparams.sample_rate},
+            },
+            ext=self.hparams.ext,
+            dummy_setting=self.hparams.dummy_setting
+        )
+
+        # g = torch.Generator()
+        # g.manual_seed(0)
+
+        return torch.utils.data.DataLoader(
+            train_dataset,
+            num_workers=self.hparams.num_workers,
+            batch_size=self.hparams.batch_size,
+            # generator=g,
+            # pin_memory=True,
+            # persistent_workers=True,
+            timeout=6000,
+        )
+
+    def val_dataloader(self):
+        dafx = self._get_dafx_for_current_epoch(self.current_epoch)
+
+        val_dataset = PairedAudioDataset(
+            dafx=dafx,
+            audio_dir=self.hparams.audio_dir,
+            subset="val",
+            train_frac=self.hparams.train_frac,
+            half=self.hparams.half,
+            length=self.hparams.train_length,
+            input_dirs=self.hparams.input_dirs,
+            buffer_size_gb=self.hparams.buffer_size_gb,
+            buffer_reload_rate=self.hparams.buffer_reload_rate,
+            num_examples_per_epoch=self.hparams.train_examples_per_epoch,
+            effect_input=self.hparams.effect_input,
+            effect_output=self.hparams.effect_output,
+            random_effect_threshold=self.hparams.random_effect_threshold,
+            augmentations={},
+            ext=self.hparams.ext,
+            dummy_setting=self.hparams.dummy_setting
+        )
+
+        # g = torch.Generator()
+        # g.manual_seed(0)
+
+        return torch.utils.data.DataLoader(
+            val_dataset,
+            num_workers=self.hparams.num_workers,
+            batch_size=self.hparams.batch_size,
+            # worker_init_fn=utils.seed_worker,
+            # generator=g,
+            # pin_memory=True,
+            # persistent_workers=True,
+            timeout=60,
+        )
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+
+        # -------- Training -----------
+        parser.add_argument("--batch_size", type=int, default=8)
+        parser.add_argument("--lr", type=float, default=1e-5)
+        parser.add_argument("--recon_loss", type=str, default="mse")
+        parser.add_argument("--vae_beta", type=float, default=1.)
+
+        # --------- DAFX ------------
+        parser.add_argument("--dafx_file", type=str, default="src/dafx/mda.vst3")
+        parser.add_argument("--dafx_names", nargs="*")
+        parser.add_argument("--dafx_param_names", nargs="*", default=None)
+
+        # --------- VAE -------------
+        parser.add_argument("--num_channels", type=int, default=4)
+        parser.add_argument("--hidden_dim", nargs="*", default=(32, 9, 257))
+        parser.add_argument("--linear_layer_dim", type=int, default=1024)
+        parser.add_argument("--latent_dim", type=int, default=1024)
+        parser.add_argument("--conv_kernel", type=int, default=5)
+        parser.add_argument("--conv_padding", type=int, default=1)
+        parser.add_argument("--conv_stride", type=int, default=3)
+
+        # -------- Spectrogram ----------
+        parser.add_argument("--n_fft", type=int, default=4096)
+        parser.add_argument("--hop_length", type=int, default=2048)
+        parser.add_argument("--window_size", type=int, default=4096)
+        parser.add_argument("--return_complex", type=bool, default=True)
+
+        # ------- Dataset  -----------
+        parser.add_argument("--audio_dir", type=str, default="src/audio")
+        parser.add_argument("--ext", type=str, default="wav")
+        parser.add_argument("--input_dirs", nargs="+", default=['musdb18_24000', 'vctk_24000'])
+        parser.add_argument("--buffer_reload_rate", type=int, default=1000)
+        parser.add_argument("--buffer_size_gb", type=float, default=1.0)
+        parser.add_argument("--sample_rate", type=int, default=24_000)
+        parser.add_argument("--dsp_sample_rate", type=int, default=24_000)
+        parser.add_argument("--shuffle", type=bool, default=True)
+        parser.add_argument("--random_effect_threshold", type=float, default=0.75)
+        parser.add_argument("--train_length", type=int, default=131_072)
+        parser.add_argument("--train_frac", type=float, default=0.9)
+        parser.add_argument("--effect_input", type=bool, default=True)
+        parser.add_argument("--effect_output", type=bool, default=True)
+        parser.add_argument("--half", type=bool, default=False)
+        parser.add_argument("--train_examples_per_epoch", type=int, default=10_000)
+        parser.add_argument("--val_length", type=int, default=131_072)
+        parser.add_argument("--val_examples_per_epoch", type=int, default=100)
+        parser.add_argument("--num_workers", type=int, default=4)
+        parser.add_argument("--dummy_setting", type=bool, default=False)
+
+        return parser
