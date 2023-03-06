94f5c39e425849fe8684706da7e8793d0c442b70
Kieran Grant <kierang92@gmail.com>
2023-03-06 10:45:48 +0000

    Add more reconstruction pngs

--- src/resume_vae_training.py
+++ src/resume_vae_training.py
@@ -8,7 +8,7 @@ from pytorch_lightning.loggers import WandbLogger
 from src.models.style_transfer_vae import StyleTransferVAE
 
 SEED = 1234
-PATH_TO_CHECKPOINT = "/home/kieran/Level5ProjectAudioVAE/src/l5proj_style_vae/renudd7j/checkpoints/epoch=1212-step=758125.ckpt"
+PATH_TO_CHECKPOINT = "/home/kieran/Level5ProjectAudioVAE/src/l5proj_style_vae/z63wn50r/checkpoints/epoch=1292-step=808125.ckpt"
 
 if __name__ == "__main__":
     pl.seed_everything(SEED)
@@ -34,8 +34,8 @@ if __name__ == "__main__":
 
     system = StyleTransferVAE.load_from_checkpoint(PATH_TO_CHECKPOINT)
 
-    system.hparams.vae_beta = 1
-    system.hparams.lr = 5e-5
+    system.hparams.vae_beta = 10
+    system.hparams.lr = 1e-4
 
     # Set up trainer
     trainer = pl.Trainer.from_argparse_args(
@@ -47,7 +47,7 @@ if __name__ == "__main__":
             early_stopping
         ],
         num_sanity_val_steps=0,
-        max_epochs=1300,
+        max_epochs=1400,
         accelerator='gpu',
     )
 
