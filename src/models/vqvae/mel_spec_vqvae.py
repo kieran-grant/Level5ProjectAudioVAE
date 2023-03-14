47df107d01156e65f9e4feb0b521475ed0d2816d
Kieran Grant <kierang92@gmail.com>
2023-03-14 13:17:17 +0000

    Working MelVQVAE

--- src/models/vqvae/mel_spec_vqvae.py
+++ src/models/vqvae/mel_spec_vqvae.py
@@ -4,12 +4,15 @@ import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torchvision.datasets as datasets
-import torchvision.transforms as transforms
+from pedalboard.pedalboard import load_plugin
 
+from src.dataset.audio_dataset import AudioDataset
 from src.models.vqvae.decoder import Decoder
 from src.models.vqvae.encoder import Encoder
 from src.models.vqvae.quantizer import VectorQuantizerEMA
+from src.utils import audio_to_mel_spectrogram
+from src.wrappers.dafx_wrapper import DAFXWrapper
+from src.wrappers.null_dafx_wrapper import NullDAFXWrapper
 
 
 class MelSpecVQVAE(pl.LightningModule):
@@ -18,9 +21,10 @@ class MelSpecVQVAE(pl.LightningModule):
         self.save_hyperparameters()
 
         self._build_model()
+        self._build_dafx()
 
     def _build_model(self):
-        self._encoder = Encoder(3, self.hparams.num_hiddens,
+        self._encoder = Encoder(1, self.hparams.num_hiddens,
                                 self.hparams.num_residual_layers,
                                 self.hparams.num_residual_hiddens)
 
@@ -39,6 +43,34 @@ class MelSpecVQVAE(pl.LightningModule):
                                 self.hparams.num_residual_layers,
                                 self.hparams.num_residual_hiddens)
 
+    def _build_dafx(self):
+        # Load instances for each type of DAFX
+        self.dafx_list = self._get_dafx_from_names()
+        # Create entry for current dafx name for logging
+        self.current_dafx = None
+
+    def _get_dafx_for_current_epoch(self, current_epoch: int):
+        # Use mod arithmetic to cycle through dafx
+        idx = current_epoch % len(self.dafx_list)
+
+        self.current_dafx = self.hparams.dafx_names[idx]
+
+        print(f"\nEpoch {current_epoch} using DAFX: {self.current_dafx}")
+
+        return self.dafx_list[idx]
+
+    def _get_dafx_from_names(self):
+        dafx_instances = []
+
+        for dafx_name in self.hparams.dafx_names:
+            if dafx_name.lower() == "clean":
+                dafx_instances.append(NullDAFXWrapper())
+            else:
+                dafx = load_plugin(self.hparams.dafx_file, plugin_name=dafx_name)
+                dafx_instances.append(DAFXWrapper(dafx, sample_rate=self.hparams.sample_rate))
+
+        return dafx_instances
+
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
 
@@ -50,73 +82,163 @@ class MelSpecVQVAE(pl.LightningModule):
 
         return loss, x_recon, perplexity
 
-    def common_step(self, batch, batch_idx, train=False):
-        data, _ = batch
-        vq_loss, data_recon, perplexity = self(data)
-        recon_error = F.mse_loss(data_recon, data)
+    def common_step(
+            self,
+            batch: torch.Tensor,
+            batch_idx: int,
+            train: bool = False,
+    ):
+        x = batch
 
-        loss = recon_error + vq_loss
+        # Get spectrograms
+        X = audio_to_mel_spectrogram(signal=x,
+                                     sample_rate=self.hparams.sample_rate,
+                                     n_mels=self.hparams.n_mels,
+                                     n_fft=self.hparams.n_fft,
+                                     win_length=self.hparams.win_length,
+                                     f_max=self.hparams.f_max,
+                                     f_min=self.hparams.f_min)
+
+        vq_loss, X_hat, perplexity = self(X)
+        recon_loss = F.mse_loss(X_hat, X)
+
+        loss = recon_loss + vq_loss
 
         self.log(("train" if train else "val") + "_loss/loss", loss)
-        self.log(("train" if train else "val") + "_loss/recon_loss", recon_error)
+        self.log(("train" if train else "val") + "_loss/recon_loss", recon_loss)
         self.log(("train" if train else "val") + "_loss/vq_loss", vq_loss)
+        self.log(("train" if train else "val") + "_loss/perplexity", perplexity)
 
-        return loss
+        data_dict = {
+            "x": X.cpu(),
+            "x_hat": X_hat.cpu(),
+            "dafx": self.current_dafx.split()[-1]
+        }
+
+        return loss, data_dict
 
     def training_step(self, batch, batch_idx):
-        loss = self.common_step(batch, batch_idx, True)
+        loss, _ = self.common_step(
+            batch,
+            batch_idx,
+            train=True)
 
         return loss
 
     def validation_step(self, batch, batch_idx):
-        loss = self.common_step(batch, batch_idx, False)
+        _, data_dict = self.common_step(
+            batch,
+            batch_idx,
+            train=False)
 
-        return loss
+        return data_dict
 
     def train_dataloader(self):
-        training_data = datasets.CIFAR10(root="data", train=True, download=True,
-                                         transform=transforms.Compose([
-                                             transforms.ToTensor(),
-                                             transforms.Normalize((0.5, 0.5, 0.5), (1.0, 1.0, 1.0))
-                                         ]))
+        dafx = self._get_dafx_for_current_epoch(self.current_epoch)
 
-        return torch.utils.data.DataLoader(training_data,
-                                           num_workers=self.hparams.num_workers,
-                                           batch_size=self.hparams.batch_size,
-                                           shuffle=True,
-                                           pin_memory=True)
+        train_dataset = AudioDataset(
+            dafx=dafx,
+            audio_dir=self.hparams.audio_dir,
+            subset="train",
+            train_frac=self.hparams.train_frac,
+            half=self.hparams.half,
+            length=self.hparams.train_length,
+            input_dirs=self.hparams.input_dirs,
+            buffer_size_gb=self.hparams.buffer_size_gb,
+            buffer_reload_rate=self.hparams.buffer_reload_rate,
+            num_examples_per_epoch=self.hparams.train_examples_per_epoch,
+            effect_audio=self.hparams.effect_audio,
+            random_effect_threshold=self.hparams.random_effect_threshold,
+            augmentations={
+                "pitch": {"sr": self.hparams.sample_rate},
+                "tempo": {"sr": self.hparams.sample_rate},
+            },
+            ext=self.hparams.ext,
+            dummy_setting=self.hparams.dummy_setting
+        )
+
+        return torch.utils.data.DataLoader(
+            train_dataset,
+            num_workers=self.hparams.num_workers,
+            batch_size=self.hparams.batch_size,
+            timeout=6000,
+        )
 
     def val_dataloader(self):
-        validation_data = datasets.CIFAR10(root="data", train=False, download=True,
-                                           transform=transforms.Compose([
-                                               transforms.ToTensor(),
-                                               transforms.Normalize((0.5, 0.5, 0.5), (1.0, 1.0, 1.0))
-                                           ]))
+        dafx = self._get_dafx_for_current_epoch(self.current_epoch)
 
-        return torch.utils.data.DataLoader(validation_data,
-                                           num_workers=self.hparams.num_workers,
-                                           batch_size=32,
-                                           shuffle=True,
-                                           pin_memory=True)
+        val_dataset = AudioDataset(
+            dafx=dafx,
+            audio_dir=self.hparams.audio_dir,
+            subset="val",
+            train_frac=self.hparams.train_frac,
+            half=self.hparams.half,
+            length=self.hparams.val_length,
+            input_dirs=self.hparams.input_dirs,
+            buffer_size_gb=self.hparams.buffer_size_gb,
+            buffer_reload_rate=self.hparams.buffer_reload_rate,
+            num_examples_per_epoch=self.hparams.val_examples_per_epoch,
+            effect_audio=self.hparams.effect_audio,
+            random_effect_threshold=self.hparams.random_effect_threshold,
+            augmentations={},
+            ext=self.hparams.ext,
+            dummy_setting=self.hparams.dummy_setting
+        )
+
+        return torch.utils.data.DataLoader(
+            val_dataset,
+            num_workers=self.hparams.num_workers,
+            batch_size=self.hparams.batch_size,
+            timeout=60,
+        )
 
     @staticmethod
     def add_model_specific_args(parent_parser):
         parser = ArgumentParser(parents=[parent_parser], add_help=False)
 
         # -------- Training -----------
-        parser.add_argument("--batch_size", type=int, default=256)
+        parser.add_argument("--batch_size", type=int, default=12)
         parser.add_argument("--lr", type=float, default=1e-3)
+
+        # --------- DAFX ------------
+        parser.add_argument("--dafx_file", type=str, default="src/dafx/mda.vst3")
+        parser.add_argument("--dafx_names", nargs="*")
+        parser.add_argument("--dafx_param_names", nargs="*", default=None)
+
+        # -------- Spectrogram ----------
+        parser.add_argument("--n_mels", type=int, default=256)
+        parser.add_argument("--n_fft", type=int, default=4096)
+        parser.add_argument("--win_length", type=int, default=1024)
+        parser.add_argument("--f_max", type=int, default=12_000)
+        parser.add_argument("--f_min", type=int, default=20)
 
         # --------- VAE -------------
         parser.add_argument("--num_hiddens", type=int, default=128)
         parser.add_argument("--num_residual_hiddens", type=int, default=32)
         parser.add_argument("--num_residual_layers", type=int, default=2)
-        parser.add_argument("--embedding_dim", type=int, default=64)
-        parser.add_argument("--num_embeddings", type=int, default=512)
+        parser.add_argument("--embedding_dim", type=int, default=128)
+        parser.add_argument("--num_embeddings", type=int, default=1024)
         parser.add_argument("--commitment_cost", type=float, default=.25)
         parser.add_argument("--decay", type=float, default=.99)
 
         # ------- Dataset  -----------
+        parser.add_argument("--audio_dir", type=str, default="src/audio")
+        parser.add_argument("--ext", type=str, default="wav")
+        parser.add_argument("--input_dirs", nargs="+", default=['musdb18_24000', 'vctk_24000'])
+        parser.add_argument("--buffer_reload_rate", type=int, default=1000)
+        parser.add_argument("--buffer_size_gb", type=float, default=1.0)
+        parser.add_argument("--sample_rate", type=int, default=24_000)
+        parser.add_argument("--dsp_sample_rate", type=int, default=24_000)
+        parser.add_argument("--shuffle", type=bool, default=True)
+        parser.add_argument("--random_effect_threshold", type=float, default=0.)
+        parser.add_argument("--train_length", type=int, default=130_560)
+        parser.add_argument("--train_frac", type=float, default=0.9)
+        parser.add_argument("--effect_audio", type=bool, default=True)
+        parser.add_argument("--half", type=bool, default=False)
+        parser.add_argument("--train_examples_per_epoch", type=int, default=2_500)
+        parser.add_argument("--val_length", type=int, default=130_560)
+        parser.add_argument("--val_examples_per_epoch", type=int, default=250)
         parser.add_argument("--num_workers", type=int, default=4)
+        parser.add_argument("--dummy_setting", type=bool, default=False)
 
         return parser
---
+++ src/train_mel_vqvae.py
@@ -0,0 +1,85 @@
+from argparse import ArgumentParser
+
+import pytorch_lightning as pl
+import torch
+import wandb
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import WandbLogger
+
+from src.callbacks.spectrogram_callback import LogSpectrogramCallback
+from src.models.vqvae.mel_spec_vqvae import MelSpecVQVAE
+
+DAFX_TO_USE = [
+    # 'mda MultiBand',
+    # 'clean',
+    'mda Delay',
+    'mda Overdrive',
+    # 'mda Ambience',
+    'mda RingMod',
+    # 'mda Leslie',
+    # 'mda Combo',
+    # 'mda Thru-Zero Flanger',
+    # 'mda Loudness',
+    # 'mda Limiter',
+    'mda Dynamics',
+]
+
+SEED = 123
+MAX_EPOCHS = 100
+
+if __name__ == "__main__":
+    wandb.require("service")
+    pl.seed_everything(SEED)
+    torch.set_float32_matmul_precision('medium')
+
+    # arg parse for config
+    parser = ArgumentParser()
+
+    # Add available trainer args and system args
+    parser = pl.Trainer.add_argparse_args(parser)
+    parser = MelSpecVQVAE.add_model_specific_args(parser)
+
+    # Parse
+    args = parser.parse_args()
+
+    # callbacks
+    wandb_logger = WandbLogger(name=f'vtck_{len(DAFX_TO_USE)}fx', project='l5proj_MelSpecVQVAE')
+
+    val_checkpoint = ModelCheckpoint(
+        monitor="val_loss/loss",
+        filename="{epoch}-{step}",
+        mode="min"
+    )
+
+    # Change settings for training
+    args.input_dirs = ['vctk_24000']
+
+    args.dafx_file = "/home/kieran/Level5ProjectAudioVAE/src/dafx/mda.vst3"
+    args.dafx_names = DAFX_TO_USE
+    args.audio_dir = "/home/kieran/Level5ProjectAudioVAE/src/audio"
+
+    # Set up trainer
+    trainer = pl.Trainer.from_argparse_args(
+        args,
+        reload_dataloaders_every_n_epochs=1,
+        logger=wandb_logger,
+        callbacks=[
+            LogSpectrogramCallback(),
+            val_checkpoint,
+            # early_stopping
+        ],
+        num_sanity_val_steps=0,
+        max_epochs=MAX_EPOCHS,
+        accelerator='gpu',
+        gradient_clip_val=5.
+    )
+
+    # create the System
+    system = MelSpecVQVAE(**vars(args))
+
+    print(system)
+    #
+    # print(torchsummary.summary(system, input_size=(1, 256, 256), device='cpu'))
+
+    # train!
+    trainer.fit(system)
