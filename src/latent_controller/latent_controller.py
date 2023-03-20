31fefdd8f6ad9965f3f9c8a2fab74cec679be325
Kieran Grant <kierang92@gmail.com>
2023-03-20 12:02:04 +0000

    Just use global vars

--- src/latent_controller/latent_controller.py
+++ src/latent_controller/latent_controller.py
@@ -1,4 +1,4 @@
-import argparse
+from typing import Optional
 
 import numpy as np
 import pandas as pd
@@ -7,35 +7,22 @@ import plotly.io as pio
 import streamlit as st
 from streamlit_plotly_events import plotly_events
 
-pio.templates.default = "plotly"
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument('--checkpoint_id', type=str, default="fboro0y2")
-n_supervised_choices = (0, 5, 10, 25, 100)
-parser.add_argument('--n_supervised', choices=n_supervised_choices, default=0,
-                    help='Number of supervised datapoints (default: %(default)s)')
-parser.add_argument('--data_dir', type=str, default="/home/kieran/Level5ProjectAudioVAE/src/latent_controller/data")
-
-args = parser.parse_args()
-
 
 @st.cache_data
-def get_nearest_point(x, y, df, n_sup):
+def get_nearest_point(x: float, y: float, df: pd.DataFrame, n_sup: int):
     data = np.array([df[f'x_emb_n={n_sup}'], df[f'y_emb_n={n_sup}']]).T
     point = np.array([x, y])
     dists = np.linalg.norm(data - point, axis=1)
-    idx = np.argmin(dists)
-    return idx
+    return np.argmin(dists)
 
 
-@st.cache_data
-def get_df(df_file):
+@st.cache_resource
+def get_df(df_file: str):
     return pd.read_csv(df_file)
 
 
 @st.cache_resource
-def get_fig(df, n_supervised, colour):
+def get_fig(df: pd.DataFrame, n_supervised: int, colour: Optional[str]):
     fig = px.scatter(df,
                      x=f"x_emb_n={n_supervised}",
                      y=f"y_emb_n={n_supervised}",
@@ -56,25 +43,57 @@ def get_fig(df, n_supervised, colour):
     return fig
 
 
-def get_audio_for_index(idx, df):
+def get_audio_file_for_index(idx: int, df: pd.DataFrame):
     return df.iloc[idx]['audio_file']
 
 
-DIR = f"{args.data_dir}/{args.checkpoint_id}/"
+def get_audio_for_file(filename: str):
+    audio = open(filename, 'rb')
+    return audio.read()
 
+
+def get_audio_for_index(idx: int, df: pd.DataFrame):
+    a_file = get_audio_file_for_index(idx, df)
+    return get_audio_for_file(a_file)
+
+
+def create_colour_options(df: pd.DataFrame):
+    colour_options = [None]
+    # Add params to colour options
+    colour_options.extend([col for col in df.columns.tolist() if "p_" in col])
+    # Add params for brightness/depth
+    colour_options.extend([col for col in df.columns.tolist() if "_diff" in col])
+
+    return colour_options
+
+
+### ===== GLOBAL VARS =======
+
+N_SUPERVISED_CHOICES = (0, 5, 10, 25, 100)
+DATA_DIRECTORY = "/home/kieran/Level5ProjectAudioVAE/src/latent_controller/data"
+EFFECT_TO_CHKPT_MAP = {
+    "Overdrive": "fboro0y2"
+}
+DEFAULT_EFFECT = "Overdrive"
+
+pio.templates.default = "plotly"
+
+# Title
 st.title("Latent Controller")
 
-n_supervised = st.selectbox("Number of supervised points",
-                            n_supervised_choices,
-                            n_supervised_choices.index(args.n_supervised))
+# Dropdown box for effect
+dafx = st.selectbox("Effect", EFFECT_TO_CHKPT_MAP.keys(), 0)
+CURRENT_EFFECT = dafx
+CHECKPOINT_ID = EFFECT_TO_CHKPT_MAP.get(CURRENT_EFFECT)
+
+DIR = f"{DATA_DIRECTORY}/{CHECKPOINT_ID}/"
+
+# Dropdown box for num supervised points
+n_supervised = st.selectbox("Number of supervised points", N_SUPERVISED_CHOICES, 0)
 
 df = get_df(f"{DIR}/full_data.csv")
 
-colour_options = [None]
-# Add params to colour options
-colour_options.extend([col for col in df.columns.tolist() if "p_" in col])
-# Add params for brightness/depth
-colour_options.extend([col for col in df.columns.tolist() if "_diff" in col])
+colour_options = create_colour_options(df)
 
 colour = st.selectbox("Parameter coloring",
                       colour_options,
@@ -85,21 +104,17 @@ selected_points = plotly_events(fig)
 
 try:
     a = selected_points[0]
-    # a = pd.DataFrame.from_dict(a,orient='index')
     index = get_nearest_point(a['x'], a['y'], df, n_supervised)
-    filename = get_audio_for_index(index, df)
 
     # Show clean audio
     st.markdown("Clean audio")
-    clean_audio = open(f"{DIR}/audio/audio_clean.wav", 'rb')
-    clean_audio_bytes = clean_audio.read()
-    st.audio(clean_audio_bytes, format='audio/wav')
+    st.audio(get_audio_for_file(f"{DIR}/audio/audio_clean.wav"))
 
     # Show effected audio
     st.markdown("Effected audio")
-    effected_audio_file = open(f"{DIR}/audio/{filename}", 'rb')
-    effected_audio_bytes = effected_audio_file.read()
-    st.audio(effected_audio_bytes, format='audio/wav')
+    f_name = get_audio_file_for_index(index, df)
+    st.audio(get_audio_for_file(f"{DIR}/audio/{f_name}"), format='audio/wav')
+
 except IndexError:
     # selected points will be empty when page first loads
     pass
