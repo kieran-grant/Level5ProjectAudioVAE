9f85e8d0f626082741df8ecce3f5fedafd1d4684
Kieran Grant <kierang92@gmail.com>
2023-03-27 14:06:09 +0100

    Start evaluation analysis

---
+++ src/evaluation/metrics_to_dataframe.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+import glob
+import re
+
+RESULTS_DIR = "/home/kieran/Level5ProjectAudioVAE/src/evaluation/data/metrics"
+DATASETS = ["daps", "vctk", "musdb18"]
+
+files = glob.glob(f"{RESULTS_DIR}/*.csv")
+
+for dataset in DATASETS:
+    dataset_files = [fl for fl in files if dataset in fl]
+    dataframes = []
+    for file_name in dataset_files:
+        # Extract the name of the effect using regex
+        match = re.search(r'/(?P<effect>[^/]+)_' + dataset + '_', file_name)
+
+        try:
+            effect_name = match.group('effect')
+        except:
+            continue
+
+        df = pd.read_csv(file_name, index_col=0)
+        df['dafx'] = effect_name
+        df.columns = [col.lower() for col in df.columns]
+        cols = list(df.columns)
+        cols = [cols[-1]] + cols[:-1]
+        df = df[cols]
+        dataframes.append(df)
+
+    df = pd.concat(dataframes)
+    df.to_csv(f"{RESULTS_DIR}/{dataset}.csv")
